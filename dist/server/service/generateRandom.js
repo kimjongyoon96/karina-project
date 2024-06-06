d88ad078b69782594e55f4f9b682f0d1669cc230
kimjongyoon96 <kimjongyoon96@gmail.com>
2024-06-06 13:06:35 +0900

    번들링의 주체를 누구로 할것인가?

--- dist/server/service/generateRandom.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const generateRandomNumber = () => {
-    const min = 100000;
-    const max = 999999;
-    const randomCode = Math.floor(Math.random() * (max - min + 1)) + min;
-    return randomCode.toString();
-};
-exports.default = generateRandomNumber;
